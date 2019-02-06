a01963e8034e0552a838cfe196ef37ae1fcefe60
Henrik Kühl <henrikkyhl@hotmail.com>
2019-02-06 20:58:40 +0100

    Initial commit

---
+++ TopicBasedPublisher/Publisher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace TopicBasedPublisher
+{
+    class Publisher
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                string topic = "";
+                Console.WriteLine("Enter a topic: ");
+
+                while ((topic = Console.ReadLine()) != "Quit")
+                {
+                    var message = new TextMessage { Text = topic };
+                    bus.Publish<TextMessage>(message, topic);
+                    Console.WriteLine("Enter a new topic: ");
+                }
+            }
+        }
+    }
+}
---
+++ TopicBasedSubscriber/Subscriber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace TopicBasedSubscriber
+{
+    class Subscriber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter subscriber id:");
+            string id = Console.ReadLine();
+            Console.WriteLine("Enter a topic: ");
+            string topic = Console.ReadLine();
+
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Subscribe<TextMessage>("subscriber" + id, HandleTextMessage, 
+                                           x => x.WithTopic(topic));
+                Console.WriteLine("Listening for messages.");
+                Console.ReadLine();
+            }
+
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: " + textMessage.Text);
+            Console.ResetColor();
+        }
+    }
+}
