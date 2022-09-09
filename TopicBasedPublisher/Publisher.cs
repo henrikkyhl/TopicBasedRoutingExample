96f0beae53601354468bcfedf87b6b53b16880f5
henrikkyhl <henrikkyhl@hotmail.com>
2022-09-09 14:48:11 +0200

    Refactored to use top-level statements

--- TopicBasedPublisher/Publisher.cs
+++ TopicBasedPublisher/Publisher.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using EasyNetQ;
+﻿using EasyNetQ;
 using Messages;
 
-namespace TopicBasedPublisher
+using (var bus = RabbitHutch.CreateBus("host=localhost"))
 {
-    class Publisher
-    {
-        static void Main(string[] args)
-        {
-            using (var bus = RabbitHutch.CreateBus("host=localhost"))
-            {
-                string topic = "";
-                Console.WriteLine("Enter a topic: ");
+    string topic = "";
+    Console.WriteLine("Enter a topic: ");
 
-                while ((topic = Console.ReadLine()) != "Quit")
-                {
-                    var message = new TextMessage { Text = topic };
-                    bus.PubSub.Publish<TextMessage>(message, topic);
-                    Console.WriteLine("Enter a new topic: ");
-                }
-            }
-        }
+    while ((topic = Console.ReadLine()) != "Quit")
+    {
+        var message = new TextMessage { Text = topic };
+        bus.PubSub.Publish<TextMessage>(message, topic);
+        Console.WriteLine("Enter a new topic: ");
     }
 }
--- TopicBasedSubscriber/Subscriber.cs
+++ TopicBasedSubscriber/Subscriber.cs
@@ -1,33 +1,23 @@
-﻿using System;
-using EasyNetQ;
+﻿using EasyNetQ;
 using Messages;
 
-namespace TopicBasedSubscriber
+Console.WriteLine("Enter subscriber id:");
+string id = Console.ReadLine();
+Console.WriteLine("Enter a topic: ");
+string topic = Console.ReadLine();
+
+using (var bus = RabbitHutch.CreateBus("host=localhost"))
 {
-    class Subscriber
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter subscriber id:");
-            string id = Console.ReadLine();
-            Console.WriteLine("Enter a topic: ");
-            string topic = Console.ReadLine();
+    bus.PubSub.Subscribe<TextMessage>("subscriber" + id, HandleTextMessage, 
+                                x => x.WithTopic(topic));
+    Console.WriteLine("Listening for messages.");
+    Console.ReadLine();
+}
 
-            using (var bus = RabbitHutch.CreateBus("host=localhost"))
-            {
-                bus.PubSub.Subscribe<TextMessage>("subscriber" + id, HandleTextMessage, 
-                                           x => x.WithTopic(topic));
-                Console.WriteLine("Listening for messages.");
-                Console.ReadLine();
-            }
 
-        }
-
-        static void HandleTextMessage(TextMessage textMessage)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Got message: " + textMessage.Text);
-            Console.ResetColor();
-        }
-    }
+void HandleTextMessage(TextMessage textMessage)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Got message: " + textMessage.Text);
+    Console.ResetColor();
 }
