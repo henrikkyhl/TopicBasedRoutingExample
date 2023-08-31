c11b421488399d00943d31dbe0ee1b748adbb5db
henrikkyhl <henrikkyhl@hotmail.com>
2023-08-31 13:14:10 +0200

    Dependencies updated

--- TopicBasedSubscriber/Subscriber.cs
+++ TopicBasedSubscriber/Subscriber.cs
@@ -1,12 +1,15 @@
 ﻿using EasyNetQ;
 using Messages;
 
+string connectionStr =
+    "host=hare.rmq.cloudamqp.com;virtualHost=npaprqop;username=npaprqop;password=<type your password here>";
+
 Console.WriteLine("Enter subscriber id:");
 string id = Console.ReadLine();
 Console.WriteLine("Enter a topic: ");
 string topic = Console.ReadLine();
 
-using (var bus = RabbitHutch.CreateBus("host=localhost"))
+using (var bus = RabbitHutch.CreateBus(connectionStr))
 {
     bus.PubSub.Subscribe<TextMessage>("subscriber" + id, HandleTextMessage, 
                                 x => x.WithTopic(topic));
